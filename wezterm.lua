0bd744839606532f46432ddecea1836e477b751c
Y.S <vv.ys.ma@gmail.com>
2025-03-03 14:24:36 +0800

    Mon Mar  3 14:24:36 CST 2025

--- wezterm.lua
+++ wezterm.lua
@@ -158,3 +158,28 @@ return {
         color_scheme = "OneLight (Gogh)",
 
 }
+
+local font_rules = {
+        {
+                intensity = 'Bold',
+                italic = true,
+                font = wezterm.font {
+                        -- family = 'Iosevka Nerd Font Mono',
+                        family = 'SF Mono',
+                        -- weight = 'ExtraBold',
+                        weight = 'Bold',
+                        style = 'Italic',
+                },
+        },
+        {
+                intensity = 'Bold',
+                italic = false,
+                font = wezterm.font {
+                        -- family = 'Iosevka Nerd Font Mono',
+                        family = 'SF Mono',
+                        -- weight = 'ExtraBold',
+                        weight = 'Bold',
+                        style = 'Normal',
+                },
+        },
+}
